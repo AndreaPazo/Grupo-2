612c4854fb4169768b95a3ceb7a40f880bbb29a9
Jerquito <najle_10@hotmail.com>
2026-05-29 14:16:21 -0500

    ejercicio07 clasificar un numero

--- Jerco_Ejercicio07.py
+++ Jerco_Ejercicio07.py
@@ -1,11 +1,24 @@
-num1 = int(input("Ingresa el primer número: "))
-num2 = int(input("Ingresa el segundo número: "))
+# Pedir un número real al usuario
+num = float(input("Ingresar un número real: "))
 
-suma = num1 + num2
+# Revisamos si el número es positivo, negativo o cero
+if num > 0:
+    clasificacion = "positivo"
+elif num < 0:
+    clasificacion = "negativo"
+else:
+    clasificacion = "cero."
+print (clasificacion)
 
-print("La suma es: ", suma)
+# Indicamos si es entero o decimal
 
+if num.is_integer():
+    tipo = "entero."
+else:
+    tipo = "decimal."
 
+#Mostrar resultado
+print (f"El número {num} es {clasificacion} y {tipo}")
 
 
 
