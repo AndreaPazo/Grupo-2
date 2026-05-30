fa048cb0b030e63a7ba806b3c03e936fab78c8b8
AndreaPazo <andrealps99@gmail.com>
2026-05-29 22:15:04 -0500

    Ejercicio04: Par e impar

---
+++ Andrea_ejercicio04.py
@@ -0,0 +1,19 @@
+#PAR O IMPAR
+# Solicitar un número entero al usuario y determinar si es par o impar. 
+# Mostrar también si el número es positivo, negativo o cero.
+
+num = int(input("Ingrese un número: "))
+
+if num % 2 == 0:
+    print (f"El número {num} es par.")
+else:
+    print(f"El número {num} es impar.")
+
+#Evaluamos si el numero es negativo, positivo o cero
+if num < 0:
+    print(f"El número {num} es negativo.")
+elif num > 0:
+    print(f"El número {num} es positivo.")
+else:
+    print(f"El número {num} es cero.")
+
