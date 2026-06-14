09a18d5c0cbaada99f0e4d89e11d88599e0d1abf
Jerquito <najle_10@hotmail.com>
2026-06-14 11:29:42 -0500

    Usar función void sin retornar ningun valor.

---
+++ S4_Ejercicios_Funciones/Ejercicio01_Funcion_void.py
@@ -0,0 +1,13 @@
+# Crea una función void que reciba un nombre y muestre un saludo en pantalla
+# No debe retornar ningún valor
+
+def saludar(nombre):
+    print(f"¡Hola, {nombre}! Bienvenido/a a la programación en Python.")
+
+# Algunos ejemplos de su uso
+saludar("Jerco")
+saludar("Dan")
+saludar("Ruth")
+saludar("Andrea")
+saludar("Grupo 2")
+
