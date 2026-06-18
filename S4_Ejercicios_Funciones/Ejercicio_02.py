40a7ab744b71d977e1db23de9aa8b401557330ca
AndreaPazo <andrealps99@gmail.com>
2026-06-17 23:48:10 -0500

    Ejercicio_02: Suma

---
+++ S4_Ejercicios_Funciones/Ejercicio_02.py
@@ -0,0 +1,8 @@
+# Crea una función que reciba dos enteros y retorne su suma. 
+# Luego muestra el resultado en el programa principal.
+
+def sumar(num1, num2):
+    resultado = num1 + num2
+    return resultado
+total = sumar(10, 20)
+print(f"La suma es: {total}")
