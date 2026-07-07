fff9ddaa81b02e41e057777d28acde09df76f012
Jerquito <najle_10@hotmail.com>
2026-07-07 13:59:51 -0500

    Transformar grados de temperatura

--- S2_Ejercicios_Basicos/Dan_Ejercicio03.py
+++ S2_Ejercicios_Basicos/Dan_Ejercicio03.py
@@ -1,5 +1,5 @@
 """Se pide ingresar grados celsius y transformarlos a Farenheit y Kelvin
-Para Farenheit se multiplica por 9/5 + 32 y para Celsius se suma +273.15"""
+Para Farenheit se multiplica por 9/5 + 32 y para Celsius se suma +273.15""" 
 celcius = float(input("Ingrese la temperatura en Celcius: "))
 faren=celcius*9/5+32
 kelvin=celcius+273.15
